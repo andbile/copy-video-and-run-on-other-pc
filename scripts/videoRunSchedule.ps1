f7b49e9810d51e1b52ed40a98231771fccd3ad08
Andrii Biletskyi <andbile@gmail.com>
2021-07-02 15:23:35 +0300

    Fix delete $video_destination path

--- scripts/videoRunSchedule.ps1
+++ scripts/videoRunSchedule.ps1
@@ -5,18 +5,21 @@ $process_id | Out-Host
 # убиваем процес
 if($process_id){
     Stop-Process -Id $process_id -Force
-    #Start-Sleep -Seconds 2
+    Start-Sleep -Seconds 1
 }
 
-
-$video_source = '\\share\_video'
+$video_source = '\\share\_video\'
 $video_destination = 'C:\script\video\'
 
-# очистка папки
-Remove-Item -Path $video_destination -Recurse
+
+# создаем папку, если она отсутствует
 if (!(Test-Path -Path $video_destination)){
     New-Item -Path $video_destination -ItemType Directory
 }
+
+
+# очистка папки
+Remove-Item -Path ($video_destination + '*.*') -Force -Recurse
 
 
 #Copy-Item -Filter *.mp4 -Path $video_source -Destination $video_destination -Force
